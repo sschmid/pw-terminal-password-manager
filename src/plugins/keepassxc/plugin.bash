bd8bf512719502b3320e03b76da304acf1f76027
Simon Schmid <mail@sschmid.com>
2024-05-13 13:17:20 +0200

    Add and use _keepassxc-cli macros

--- src/plugins/keepassxc/plugin.bash
+++ src/plugins/keepassxc/plugin.bash
@@ -1,25 +1,49 @@
 : "${PW_KEEPASSXC:="/Applications/KeePassXC.app/Contents/MacOS/keepassxc-cli"}"
 
-_get_password() {
+_set_password() {
   if [[ ! -v PW_KEEPASSXC_PASSWORD ]]; then
     read -rsp "Enter password for ${PW_KEYCHAIN}: " PW_KEEPASSXC_PASSWORD </dev/tty
     echo
   fi
 }
 
+_keepassxc-cli() {
+  "${PW_KEEPASSXC}" "$@"
+}
+
+_keepassxc-cli_with_db_password() {
+  _set_password
+  _keepassxc-cli "$@" <<< "${PW_KEEPASSXC_PASSWORD}"
+}
+
+_keepassxc-cli_with_db_password_and_entry_password() {
+  _set_password
+  local password="$1"; shift
+  _keepassxc-cli "$@" << EOF
+${PW_KEEPASSXC_PASSWORD}
+${password}
+EOF
+}
+
 PW_NAME=""
 declare -ig PW_FZF=0
 
-pw::init() { "${PW_KEEPASSXC}" db-create -p "${PW_KEYCHAIN}"; }
+pw::init() { _keepassxc-cli db-create -p "${PW_KEYCHAIN}"; }
 pw::open() { open -a "KeePassXC" "${PW_KEYCHAIN}"; }
 pw::lock() { echo "not available for keepassxc-cli"; }
-pw::unlock() {
-  _get_password
-  "${PW_KEEPASSXC}" open -q "${PW_KEYCHAIN}" <<< "${PW_KEEPASSXC_PASSWORD}"
-}
+pw::unlock() { _keepassxc-cli open "${PW_KEYCHAIN}"; }
 
 pw::add() {
-  _get_password
+  _addOrEdit 0 "$@"
+}
+
+pw::edit() {
+  pw::select_entry_with_prompt edit "$@"
+  _addOrEdit 1 "${PW_NAME}"
+}
+
+_addOrEdit() {
+  local -i edit=$1; shift
   PW_NAME="$1" account="${2:-}"
   local password retype
   read -rsp "Enter password for ${PW_NAME}: " password; echo
@@ -32,35 +56,21 @@ pw::add() {
   else
     password="$(pw::gen 1)"
   fi
-  echo -ne "${PW_KEEPASSXC_PASSWORD}\n${password}\n" | "${PW_KEEPASSXC}" add -qp "${PW_KEYCHAIN}" -u "${account}" "${PW_NAME}"
-}
 
-pw::edit() {
-  _get_password
-  PW_NAME="$1"
-  local password retype
-  read -rsp "Enter password for ${PW_NAME}: " password; echo
-  if [[ -n "${password}" ]]; then
-    read -rsp "Retype password for ${PW_NAME}: " retype; echo
-    if [[ "${retype}" != "${password}" ]]; then
-      echo "Error: the entered passwords do not match."
-      exit 1
-    fi
-  else
-    password="$(pw::gen 1)"
+  if ((edit))
+  then _keepassxc-cli_with_db_password_and_entry_password "${password}" edit -qp "${PW_KEYCHAIN}" "${PW_NAME}"
+  else _keepassxc-cli_with_db_password_and_entry_password "${password}" add -qp "${PW_KEYCHAIN}" -u "${account}" "${PW_NAME}"
   fi
-  echo -ne "${PW_KEEPASSXC_PASSWORD}\n${password}\n" | "${PW_KEEPASSXC}" edit -qp "${PW_KEYCHAIN}" "${PW_NAME}"
 }
 
 pw::get() {
-  _get_password
   local -i print=$1; shift
   if ((print))
   then pw::select_entry_with_prompt print "$@"
   else pw::select_entry_with_prompt copy "$@"
   fi
   local password
-  password="$("${PW_KEEPASSXC}" show -qsa Password "${PW_KEYCHAIN}" "${PW_NAME}" <<< "${PW_KEEPASSXC_PASSWORD}")"
+  password="$(_keepassxc-cli_with_db_password show -qsa Password "${PW_KEYCHAIN}" "${PW_NAME}")"
   if ((print)); then
     echo "${password}"
   else
@@ -76,24 +86,23 @@ pw::get() {
 }
 
 pw::rm() {
-  _get_password
   local -i remove=1
   pw::select_entry_with_prompt remove "$@"
   if ((PW_FZF)); then
     read -rp "Do you really want to remove ${PW_NAME} from ${PW_KEYCHAIN}? (y / n): "
     [[ "${REPLY}" == "y" ]] || remove=0
   fi
-  ((!remove)) || "${PW_KEEPASSXC}" rm -q "${PW_KEYCHAIN}" "${PW_NAME}" <<< "${PW_KEEPASSXC_PASSWORD}"
+  ((!remove)) || _keepassxc-cli_with_db_password rm -q "${PW_KEYCHAIN}" "${PW_NAME}"
 }
 
 pw::list() {
-  _get_password
   local list
-  list="$("${PW_KEEPASSXC}" ls -qfR "${PW_KEYCHAIN}" <<< "${PW_KEEPASSXC_PASSWORD}" | grep -v '/$')"
+  list="$(_keepassxc-cli_with_db_password ls -qfR "${PW_KEYCHAIN}" | grep -v '/$')"
   [[ "${list}" == "[empty]" ]] || echo "${list}"
 }
 
 pw::select_entry_with_prompt() {
+  _set_password
   local fzf_prompt="$1"; shift
   if (($#)); then
     PW_NAME="$1"
--- test/keepassxc_cli-test-helper.bash
+++ test/keepassxc_cli-test-helper.bash
@@ -6,10 +6,25 @@ load 'test_helper/bats-file/load.bash'
 TEST_KEYCHAIN="${BATS_TEST_TMPDIR}/pw_test.kdbx"
 TEST_PASSWORD="pw_test_password"
 
+_keepassxc-cli() {
+  "${PW_KEEPASSXC}" "$@"
+}
+
+_keepassxc-cli_with_db_password() {
+  _keepassxc-cli "$@" <<< "${TEST_PASSWORD}"
+}
+
+_keepassxc-cli_with_db_password_and_entry_password() {
+  local password="$1"; shift
+  _keepassxc-cli "$@" << EOF
+${TEST_PASSWORD}
+${password}
+EOF
+}
+
 _setup() {
   rm -f "${TEST_KEYCHAIN}"
-  echo -ne "${TEST_PASSWORD}\n${TEST_PASSWORD}\n" \
-  | "${PW_KEEPASSXC}" db-create -qp "${TEST_KEYCHAIN}"
+  _keepassxc-cli_with_db_password_and_entry_password "${TEST_PASSWORD}" db-create -qp "${TEST_KEYCHAIN}"
 }
 
 _teardown() {
@@ -29,7 +44,7 @@ _add_item_with_name_and_account() {
 }
 _pp_add_item_with_name_and_account() {
   local item_name="$1" item_account="$2" item_pw="$3"
-  echo -ne "${TEST_PASSWORD}\n${item_pw}\n" | "${PW_KEEPASSXC}" add -qp "${TEST_KEYCHAIN}" -u "${item_account}" "${item_name}"
+  _keepassxc-cli_with_db_password_and_entry_password "${item_pw}" add -qp "${TEST_KEYCHAIN}" -u "${item_account}" "${item_name}"
 }
 
 _update_item_with_name() { _update_item_with_name_and_account "$1" "" "$2"; }
@@ -40,7 +55,7 @@ _update_item_with_name_and_account() {
 }
 _pp_update_item_with_name() {
   local item_name="$1" item_pw="$3"
-  echo -ne "${TEST_PASSWORD}\n${item_pw}\n" | "${PW_KEEPASSXC}" edit -qp "${TEST_KEYCHAIN}" "${item_name}"
+  _keepassxc-cli_with_db_password_and_entry_password "${item_pw}" edit -qp "${TEST_KEYCHAIN}" "${item_name}"
 }
 
 _delete_item_with_name() {
@@ -49,7 +64,7 @@ _delete_item_with_name() {
 }
 _pp__delete_item_with_name() {
   local item_name="$1"
-  "${PW_KEEPASSXC}" rm -q "${TEST_KEYCHAIN}" "${item_name}" <<< "${TEST_PASSWORD}"
+  _keepassxc-cli_with_db_password rm -q "${TEST_KEYCHAIN}" "${item_name}"
 }
 
 _delete_item_with_account() {
@@ -75,7 +90,7 @@ assert_item_with_name() {
   assert_output "${item_pw}"
 }
 _pp_assert_item_with_name() {
-  "${PW_KEEPASSXC}" show -qsa Password "${TEST_KEYCHAIN}" "$1" <<< "${TEST_PASSWORD}"
+  _keepassxc-cli_with_db_password show -qsa Password "${TEST_KEYCHAIN}" "$1"
 }
 
 assert_item_with_account() {
@@ -93,7 +108,7 @@ assert_no_item_with_name() {
   assert_output "Could not find entry with path ${item_name}."
 }
 _pp_assert_no_item_with_name() {
-  "${PW_KEEPASSXC}" show -qsa Password "${TEST_KEYCHAIN}" "$1" <<< "${TEST_PASSWORD}"
+  _keepassxc-cli_with_db_password show -qsa Password "${TEST_KEYCHAIN}" "$1"
 }
 
 assert_no_item_with_account() {
@@ -110,5 +125,5 @@ assert_deleted_item_with_name() {
   assert_success
 }
 _pp_assert_deleted_item_with_name() {
-  "${PW_KEEPASSXC}" show -qsa Password "${TEST_KEYCHAIN}" "/Recycle Bin/$1" <<< "${TEST_PASSWORD}"
+  _keepassxc-cli_with_db_password show -qsa Password "${TEST_KEYCHAIN}" "/Recycle Bin/$1"
 }
