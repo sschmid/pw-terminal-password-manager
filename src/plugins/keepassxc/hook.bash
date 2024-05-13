34d9c09ab06b44223aabe0d88b0a501b7ccbfc53
Simon Schmid <mail@sschmid.com>
2024-05-13 13:14:11 +0200

    Refactor plugin hooks and infer plugin based on file extension for pw init
    fix

--- src/plugins/keepassxc/hook.bash
+++ src/plugins/keepassxc/hook.bash
@@ -1,6 +1,11 @@
-filetype() { echo "Keepass password database 2.x KDBX"; }
+FILE_TYPE="Keepass password database 2.x KDBX"
+FILE_EXTENSION="kdbx"
 
 register() {
-  [[ -f "${PW_KEYCHAIN}" && "$(file -b "${PW_KEYCHAIN}")" == "Keepass password database 2.x KDBX" ]] && return 0
-  return 1
+  [[ -f "${PW_KEYCHAIN}" ]]
+  [[ "$(file -b "${PW_KEYCHAIN}")" == "${FILE_TYPE}" ]]
+}
+
+register_with_extension() {
+  [[ "$1" == "${FILE_EXTENSION}" ]]
 }
--- src/plugins/macos_keychain/hook.bash
+++ src/plugins/macos_keychain/hook.bash
@@ -1,7 +1,11 @@
-filetype() { echo "Mac OS X Keychain File"; }
+FILE_TYPE="Mac OS X Keychain File"
+FILE_EXTENSION="keychain-db"
 
 register() {
-  [[ -f "${PW_KEYCHAIN}" && "$(file -b "${PW_KEYCHAIN}")" == "Mac OS X Keychain File" ]] && return 0
-  [[ -f "${HOME}/Library/Keychains/${PW_KEYCHAIN}" ]] && return 0
-  return 1
+  [[ -f "${PW_KEYCHAIN}" && "$(file -b "${PW_KEYCHAIN}")" == "${FILE_TYPE}" ]] && return 0
+  [[ -f "${HOME}/Library/Keychains/${PW_KEYCHAIN}" ]]
+}
+
+register_with_extension() {
+  [[ "$1" == "${FILE_EXTENSION}" ]]
 }
