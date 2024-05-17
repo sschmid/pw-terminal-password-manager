ae579f0682613e0a353672962a329afd78d07d15
Simon Schmid <mail@sschmid.com>
2024-05-17 12:39:07 +0200

    keepassxc: Exclude Recycle Bin

--- src/plugins/keepassxc/plugin.bash
+++ src/plugins/keepassxc/plugin.bash
@@ -73,7 +73,7 @@ pw::rm() {
 
 pw::list() {
   local list
-  if ! list="$(_keepassxc-cli_with_db_password ls -qfR "${PW_KEYCHAIN}" | grep -v '/$')"; then
+  if ! list="$(_keepassxc-cli_with_db_password ls -qfR "${PW_KEYCHAIN}" | grep -v -e '/$' -e 'Recycle Bin')"; then
     echo "Error while reading the database ${PW_KEYCHAIN}: Invalid credentials were provided, please try again." >&2
     exit 1
   fi
