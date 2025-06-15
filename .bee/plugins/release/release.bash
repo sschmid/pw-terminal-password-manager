4e4022550bf9dce59700d892cf2175f34f671715
Simon Schmid <mail@sschmid.com>
2025-06-15 16:38:41 +0200

    Add release::upload

--- .bee/plugins/release/release.bash
+++ .bee/plugins/release/release.bash
@@ -8,4 +8,32 @@ release::publish() {
   git tag "${version}"
   git push --tags
   github::create_release
+  release::upload "${version}"
+}
+
+release::upload() {
+  local tag="$1"
+
+  # Choose run to download artifacts from
+  local -i run_id
+  run_id="$(github::runs \
+    | jq -r '.workflow_runs[] | "\(.id) [\(.conclusion)]: \(.display_title)"' \
+    | fzf --header="Select a run to download artifacts from" \
+    | awk '{print $1}')"
+
+  # Choose artifact to download
+  local artifact
+  artifact="$(github::artifacts ${run_id} \
+    | jq -r '.artifacts[] | "\(.id) \(.name)"' \
+    | fzf --header="Select an artifact to download")"
+
+  echo "Downloading artifact"
+  mkdir -p dist
+  github::download "$(awk '{print $1}' <<< "${artifact}")" "dist/$(awk '{print $2}' <<< "${artifact}")"
+
+  local release_id
+  release_id="$(github::releases "/tags/${tag}" | jq -r '.id')"
+
+  echo "Uploading assets to release ${tag} (${release_id})"
+  github::upload_assets "${release_id}"
 }
