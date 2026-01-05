1a657596f619d87ab47dde321a0ca8ace19dc816
Simon Schmid <mail@sschmid.com>
2026-01-05 20:17:53 +0100

    Move function

--- .bee/plugins/release/release.bash
+++ .bee/plugins/release/release.bash
@@ -17,6 +17,27 @@ release::publish() {
 	_upload_assets "${version}"
 }
 
+_set_release_run_id() {
+	[[ -n "${RELEASE_RUN_ID}" ]] && return
+	local tag="$1" run run_id run_name
+	while :; do
+		echo -n "Fetching latest run: "
+		run="$(github::runs "?per_page=1" | jq -r '.workflow_runs[0] | "\(.id) \(.display_title)"')"
+		run_id="${run%% *}"
+		run_name="${run#* }"
+		echo "[${run_id}] ${run_name}"
+
+		if [[ "${run_name}" != "Release ${tag}" ]]; then
+			echo "Latest run is not 'Release ${tag}'! Retrying in 5 seconds..."
+			sleep 5
+			continue
+		fi
+
+		RELEASE_RUN_ID="${run_id}"
+		break
+	done
+}
+
 _wait_for_run() {
 	# gh cli
 	if ! gh run watch --compact --exit-status "${RELEASE_RUN_ID}"; then
@@ -46,27 +67,6 @@ _wait_for_run() {
 	# 			;;
 	# 	esac
 	# done
-}
-
-_set_release_run_id() {
-	[[ -n "${RELEASE_RUN_ID}" ]] && return
-	local tag="$1" run run_id run_name
-	while :; do
-		echo -n "Fetching latest run: "
-		run="$(github::runs "?per_page=1" | jq -r '.workflow_runs[0] | "\(.id) \(.display_title)"')"
-		run_id="${run%% *}"
-		run_name="${run#* }"
-		echo "[${run_id}] ${run_name}"
-
-		if [[ "${run_name}" != "Release ${tag}" ]]; then
-			echo "Latest run is not 'Release ${tag}'! Retrying in 5 seconds..."
-			sleep 5
-			continue
-		fi
-
-		RELEASE_RUN_ID="${run_id}"
-		break
-	done
 }
 
 _download_artifact() {
