ae770b15c44060576d69b4cc8f54379452963fe4
Simon Schmid <mail@sschmid.com>
2026-09-14 13:46:58 +0200

    Move PROGRAM to vars.bash

--- src/vars.bash
+++ src/vars.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+declare -grx PROGRAM="pw"
+
 declare -gr PW_HOME="${PW_HOME:-.}"
 declare -gr PW_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}/pw"
 declare -gr PW_CONFIG_FILE="${PW_CONFIG_HOME}/pw.conf"
